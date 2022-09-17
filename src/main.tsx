a6b61154bf4a055f85b399682a80bf22d12fc13e
IsraelCost <cmteisrael@gmail.com>
2022-09-17 12:59:41 -0300

    add other routes and react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from "./app";
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<BrowserRouter>
+  <Routes>
+    <Route path='/share/:id' element={<App />} />
+    <Route path='/share' element={<h1>Hello SHare</h1>} />
+    <Route path='/' element={<h1>Hello Home</h1>} />
+  </Routes>
+</BrowserRouter>, document.getElementById('app'))
--- src/server.ts
+++ src/server.ts
@@ -3,35 +3,41 @@ import cors from 'cors'
 
 const app = express()
 
+const getHTML = (id: string = 'Hello') => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>${id}</title>
+  <meta property="og:title" content="Mapa Interativo">
+</head>
+<body>
+  <div id="app"></div>
+  <script src="/public/main.js"></script>
+</body>
+</html>
+`
+
 app.use(cors())
 
+app.use((req, res, next) => {
+  global.window = {} as any
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(getHTML())
 })
 
 app.get('/share/:id', (req, res) => {
-  global.window = {} as any
   const { id } = req.params
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>${id}</title>
-      <meta property="og:title" content="Mapa Interativo">
-    </head>
-    <body>
-      <div id="app"></div>
-      <script src="/public/main.js"></script>
-    </body>
-    </html>
-  `
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(html)
+  res.send(getHTML(id))
 })
 
+app.get('/share', (req, res) => {
+  res.send(getHTML())
+})
 app.use(express.static('dist'))
 
 app.listen(80, () => {
